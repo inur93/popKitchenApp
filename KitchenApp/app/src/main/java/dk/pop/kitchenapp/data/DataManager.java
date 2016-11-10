144916df0214b78d7a7ded333cfa1d374806950b
dickow <Jeppedic@gmail.com>
2016-11-10 11:24:49 +0100

    Changed the getActivitiesFromKitchen Query, to use the right resource

--- KitchenApp/app/src/main/java/dk/pop/kitchenapp/data/DataManager.java
+++ KitchenApp/app/src/main/java/dk/pop/kitchenapp/data/DataManager.java
@@ -25,6 +25,8 @@ public class DataManager implements IDataManager {
     public final String KITCHENRESOURCE = "kitchens/";
     public final String PERSONRESOURCE = "persons/";
     public final String ACTIVITIESRESOURCE = "activities/";
+    public final String ACTIVITYKITCHENRESOURCE = "kitchen/";
+
     private Person currentPerson;
     private Kitchen currentKitchen;
 
@@ -200,7 +202,7 @@ public class DataManager implements IDataManager {
     @Override
     public void getActivitiesForKitchen(@NonNull Kitchen kitchen, ChildEventListener listener) {
         database.child(ACTIVITIESRESOURCE)
-                .orderByChild(String.format("%s/%s", KITCHENRESOURCE, kitchen.getName()))
+                .orderByChild(String.format("%s/%s", ACTIVITYKITCHENRESOURCE, kitchen.getName()))
                 .equalTo(kitchen.getName())
                 .addChildEventListener(listener);
     }
@@ -208,7 +210,7 @@ public class DataManager implements IDataManager {
     @Override
     public void detachActivitiesForKitchen(@NonNull Kitchen kitchen, ChildEventListener listener) {
         database.child(ACTIVITIESRESOURCE)
-                .orderByChild(String.format("%s/%s", KITCHENRESOURCE, kitchen.getName()))
+                .orderByChild(String.format("%s/%s", ACTIVITYKITCHENRESOURCE, kitchen.getName()))
                 .equalTo(kitchen.getName())
                 .removeEventListener(listener);
     }
